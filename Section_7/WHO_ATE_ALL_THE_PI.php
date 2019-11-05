2d6c718bd8dc98126cf71eddf59fd933a71b71bc
kykypyka <kykypyka@mail.ru>
2019-11-05 22:31:08 +0300

    TEST

---
+++ Section_7/WHO_ATE_ALL_THE_PI.php
@@ -0,0 +1,5 @@
+<?php
+
+$radius = 5;
+$areaOfCircle = pi()*(pow($radius, 2));
+echo "Are of a circle is ".round($areaOfCircle, 2)." square meters.";
---
+++ Section_7/date.php
@@ -0,0 +1,6 @@
+<?php
+
+date_default_timezone_get('Europe/Moscow');
+echo "The year is ". date('Y')."<br>";
+echo "Today is ".date('l')."<br>";
+echo "The time is ".date('g:i a');
